b8db3eebcd256dd69b5be43b9b2dac93609f626c
HadarSabati <75303359+HadarSabati@users.noreply.github.com>
2026-02-01 10:00:13 +0200

    Trying to build

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -11,10 +10,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Mouse.current.leftButton.isPressed)
+        if (Input.GetMouseButton(0))
+
+            //if (Mouse.current.leftButton.isPressed)
         {
+
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+
             //Camera.main.ScreenToWorldPoint() function to translate the mouse’s position from screen space to a point in the game world
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
+            //Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
             //Debug.Log("Mouse position: " + mousePos);
         }
     }
